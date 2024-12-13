7db9ebdb6a26150da6cb365a816658b15f782acc
MalikAlisha <115615154+MalikAlisha@users.noreply.github.com>
2024-12-13 10:22:39 +0530

    Updated Text

--- lib/utils/constants/strings/text_strings.dart
+++ lib/utils/constants/strings/text_strings.dart
@@ -16,27 +16,26 @@ class TextStrings {
       "For those who seek an exceptional home and life, there is only Mahalaxmi Developers";
   static String homeScreenDetail1Body =
       "Built on centuries of tradition and dedicated to innovating the luxury real estate industry, Mahalaxmi Developers offers transformative experiences through a global network of exceptional agents.";
-
   // Name of Owner
-  static String ownerName = "Name of owner";
+  static String ownerName = "Panna Laal Mishra";
   // Owner description
-  static String ownerDescription = "Owner Description";
+  static String ownerDescription = "Panna Laal Mishra is a seasoned entrepreneur and the visionary owner of a thriving real estate company. With years of experience in the real estate industry, Mr. Mishra has built a reputation for integrity, strategic planning, and a deep understanding of property markets. His company specializes in providing comprehensive real estate solutions, including residential and commercial property development, investment advisory, and property management services.";
   // owner quote
-  static String ownerQuote = "\"Quote by Owner\"";
+  static String ownerQuote = "Real estate is not just about buildings and land; it's about creating spaces where dreams take root, communities thrive, and futures are built.";
 
   // Name of Co-Owner
-  static String coOwnerName = "Name of co-owner";
+  static String coOwnerName = "Vinnit Mishra";
   // Co-Owner Description
-  static String coOwnerDescription = "Owner Description";
+  static String coOwnerDescription = "Vinnit Mishra is the co-owner of a prominent real estate company and a driving force behind its success. With a collaborative approach and a strong vision for the future, Mr. Mishra plays a pivotal role in shaping the company’s strategy, operations, and growth. As a skilled entrepreneur and innovator, Vinnit Mishra focuses on building sustainable and high-quality real estate projects that cater to the needs of modern communities. His expertise in market analysis, client relations, and project management ensures the company consistently delivers value to its customers and stakeholders.";
   // quote by coOwner
-  static String coOwnerQuote = "\"Quote by Co-Owner\"";
+  static String coOwnerQuote = "Success in real estate is built on trust, vision, and collaboration. Together, we create spaces that stand the test of time and bring dreams to life.";
 
   // Contact Messgae
   static String contactMessage =
       "Have a question or need assistance? We're here to help! Get in touch with us, and we'll respond as soon as possible.";
 
   // Address
-  static String address = "The address of the company";
+  static String address = "Mahalaxmi Developers123, Sunrise TowerNear City Center MallMG Road, Indore, Madhya Pradesh - 452001IndiaContact: +91-9876543210Email: info@mahalaxmidevelopers.";
   // address link
   static String addressLink = "www.github.com/adarshpandey18/";
   // Phone number
